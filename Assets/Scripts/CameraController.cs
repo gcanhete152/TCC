66b6fc406fa458d9d2f74578e2bd1fe00fbdf3f1
Gabriel Marcilio de Abreu Canhete <ra00195151@pucsp.edu.br>
2019-03-29 10:22:33 -0300

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;    
+    
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
+    
+    void Start()
+    {
+        if(!useOffsetValues)
+        {
+            offset = target.position-transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //update iss called once per frame
+    void LateUpdate()
+    {
+        //get the x position of the mouse & rotate the target
+        float horizontal =  Input.GetAxisRaw("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //get the y position of the mouse and rotate the pivot
+        float vertical =  Input.GetAxisRaw("Mouse Y") * rotateSpeed;
+        
+        if(invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //limit up/down camera rotation
+        if (pivot.rotation.eulerAngles.x>maxViewAngle && pivot.rotation.eulerAngles.x<180)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle,0,0);
+        }
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x <360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+        
+        //move the camera based on the current rotation of the target and the original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position=target.position-offset;        
+
+        if (transform.position.y<target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y-0.7f, transform.position.z);
+            //Mathf.Lerp(1f, 5f, 0.5f);
+        }
+    
+        transform.LookAt(target);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    //public Rigidbody theRB;
+
+    private Vector3 movementDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        print("Mechanics");
+        //theRB = GetComponent<Rigidbody>();
+        //moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Also Usefull since it acts directly on the transformation of the object independant of the ridgid body /  transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
+        //theRB.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical")*moveSpeed);
+
+        /*if(Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }*/
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), movementDirection.y, Input.GetAxis("Vertical"));
+        if (controller.isGrounded)
+        {
+            movementDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                movementDirection.y = jumpForce;
+            }
+        }
+        movementDirection.y = movementDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        Vector3 mymov = transform.TransformDirection(movementDirection);
+        controller.Move((mymov * Time.deltaTime) * moveSpeed);
+    }
+}
